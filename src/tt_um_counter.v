f126a2260ac5abc832a514d06f3894707fde3a5d
Asilvaj12345 <163932021+Asilvaj12345@users.noreply.github.com>
2024-03-18 16:27:19 -0600

    Update and rename project.v to tt_um_counter.v

--- src/tt_um_counter.v
+++ src/tt_um_counter.v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module tt_um_example (
+module tt_um_counter (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,8 +17,11 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
 
+    counter #(.bits(4)) c1 (.clk(uio_in[7]) , .rst(uio_in[6], .select(uio_in[5]), .count(uio_out[3:0]));
+
+    assign uio_oe = 8'b00001111;
+    assign uio_out[7:4] = 4'b0000;
+    assign uo_out[7:0] = 8'b00000000;
+                            
 endmodule
